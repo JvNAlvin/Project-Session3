a5cce96ae930ac49b16f1482f093e92a4fa72447
JovaN <jovanalvin@gmail.com>
2021-11-27 10:49:10 +0700

    Initial Commit

---
+++ ProjectSession3/ProjectSession3App.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectSession3App.swift
+//  ProjectSession3
+//
+//  Created by Jovan Alvin on 27/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ProjectSession3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
